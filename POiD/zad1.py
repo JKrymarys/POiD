8c2e80132c8b270a3c22ab23c888883bba0cab7e
Jakub Krymarys <krymarys.jakub@gmail.com>
2020-03-14 14:46:41 +0100

    Add generic functions

--- POiD/zad1.py
+++ POiD/zad1.py
@@ -11,11 +11,35 @@ def display_image(image_url):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-#script
+def get_image_height(img):
+    return img.shape[0]
+
+def get_image_width(img):
+    return img.shape[1]
+
+
+# def change_image_brightness():
+#     img = load_image('cat.jpg')
+#     new_image = []
+#     it = 0
+#     # print(img)
+#     # for pixel_value in img:
+#         # new_image.add(pixel_value + 10)
+    
+#     # displsay_image(new_image)
+
+
+
+
+
 try:
     img = load_image('cat.jpg')
+    print(get_image_height(img))
+    print(get_image_width(img))
+
     display_image(img)
+
 except Exception as e:
-    print(f"exception: {e}")
+    print(f"Upsik: {e}")
 
 #display_image(img)
