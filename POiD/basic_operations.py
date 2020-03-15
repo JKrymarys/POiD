fc9bad337cf4c7177fc2261c28a741616bbcefa3
Jakub Krymarys <krymarys.jakub@gmail.com>
2020-03-15 10:03:27 +0100

    Code refactor and improvements

--- POiD/basic_operations.py
+++ POiD/basic_operations.py
@@ -1,51 +1,35 @@
 import numpy as np
 import cv2
-import utils as utils
+import utils
 
 
-def increase_image_brightness(img):
+def increase_image_brightness(img,brightness_change):
     img_height = utils.get_image_height(img)
     img_width = utils.get_image_width(img)
-    new_img = []
     new_img = img.copy()
 
-    y = 0
-    x = 0
-    c = 0 
-
-    b = 50
-
-    # while y < img_height:
     for y in range(img_height):
         for x in range(img_width):
             for c in range(3):
-                if img[y][x][c] + b > 255 :
+                if img[y][x][c] + brightness_change > 255 :
                     new_img[y][x][c] = 255
                 else:
-                    new_img[y][x][c] = img[y][x][c] + b
+                    new_img[y][x][c] = img[y][x][c] + brightness_change
 
     return new_img
 
-def decrease_image_brightness(img):
+def decrease_image_brightness(img,brightness_change):
     img_height = utils.get_image_height(img)
     img_width = utils.get_image_width(img)
-    new_img = []
     new_img = img.copy()
 
-    y = 0
-    x = 0
-    c = 0 
-
-    b = 50
-
-    # while y < img_height:
     for y in range(img_height):
         for x in range(img_width):
             for c in range(3):
-                if img[y][x][c] - b < 0 :
+                if img[y][x][c] - brightness_change < 0 :
                     new_img[y][x][c] = 0
                 else:
-                    new_img[y][x][c] = img[y][x][c] - b
+                    new_img[y][x][c] = img[y][x][c] - brightness_change
 
     return new_img
 
