36c33e7179bdee1d91cc6c64c638709f9ef28736
Jakub Krymarys <krymarys.jakub@gmail.com>
2020-03-21 13:13:31 +0100

    Improve historam function

--- POiD/core.py
+++ POiD/core.py
@@ -6,32 +6,34 @@ import histogram
 import filters
 
 img = utils.load_image('cat.jpg')
+hist = histogram.create_histogram(img)
+
 
 def contrast_param_change(x):
     value = x/10
-    utils.display_image('image',basic_operations.adjust_contrast(img, value))
+    utils.display_image('image',basic_operations.adjust_contrast(img, value), hist)
 
 def brightness_param_change(x):
     value = x - 256
-    utils.display_image('image',basic_operations.adjust_brightness(img, value))
+    utils.display_image('image',basic_operations.adjust_brightness(img, value),hist)
 
 def negative_switch(x):
     if x == 1:
-        utils.display_image('image', basic_operations.create_negative(img))
+        utils.display_image('image', basic_operations.create_negative(img), hist)
     else:
-        utils.display_image('image', img)
+        utils.display_image('image', img, hist)
 
 def average_filter_change(x):
     if x == 1:
-        utils.display_image('image', filters.average_filter(img))
+        utils.display_image('image', filters.average_filter(img), hist)
     else:
-        utils.display_image('image', img)
+        utils.display_image('image', img, hist)
 
 def median_filter_change(x):
     if x == 1:
-        utils.display_image('image', filters.median_filter(img, 10))
+        utils.display_image('image', filters.median_filter(img, 10), hist)
     else:
-        utils.display_image('image', img)
+        utils.display_image('image', img, hist)
 
 
 # try:
@@ -41,8 +43,8 @@ cv2.createTrackbar('brightness','image', 256,512, brightness_param_change)
 cv2.createTrackbar('negative','image', 0,1, negative_switch)
 cv2.createTrackbar('average_filter','image', 0,1, average_filter_change)
 cv2.createTrackbar('median_filter','image', 0,1, median_filter_change)
-histogram.display_histograms(img) 
-utils.display_image('image',img)
+utils.display_image('image',img,hist)
+hist.show();
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 # except Exception as e:
