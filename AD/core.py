15e148b8dadb7eb8d982cdbf6d4850ba6c9a0779
Paulina Wyrwas <paulina.p.wyrwas@gmail.com>
2020-03-28 16:01:38 +0100

    function names fixes

--- AD/core.py
+++ AD/core.py
@@ -40,10 +40,10 @@ def display_hist(data, cols):
     plt.title("Iris Histogram")
     plt.show()
 
-print("Median of attributes:\n\n", median(data[["sepal_length", "sepal_width", "petal_length", "petal_width"]]))
-print("\nMin of attributes:\n\n", min(data[["sepal_length", "sepal_width", "petal_length", "petal_width"]]))
-print("\nMax of attributes:\n\n", max(data[["sepal_length", "sepal_width", "petal_length", "petal_width"]]))
-print("\nDominant: ", dominant(data["class"]))
+print("Median of attributes:\n\n", calculate_median(data[["sepal_length", "sepal_width", "petal_length", "petal_width"]]))
+print("\nMin of attributes:\n\n", get_min(data[["sepal_length", "sepal_width", "petal_length", "petal_width"]]))
+print("\nMax of attributes:\n\n", get_max(data[["sepal_length", "sepal_width", "petal_length", "petal_width"]]))
+print("\nDominant: ", get_dominant(data["class"]))
 
 #Narysować histogramy dla dwóch cech ilościowych najbardziej ze sobą skorelowanych
 
@@ -53,4 +53,4 @@ print("\nCorrelations:\n\n", data.corr())
 print("\nMost correlated pair:\n\n", correlation_data(data[["sepal_length", "sepal_width", "petal_length", "petal_width"]]))
 
 cor_column_names = correlation_data(data[["sepal_length", "sepal_width", "petal_length", "petal_width"]]).index[0]
-hist(data, cor_column_names)	
+display_hist(data, cor_column_names)	
