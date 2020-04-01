89bf835a97047ac150ec60f6694fab5927ca161e
Paulina Wyrwas <paulina.p.wyrwas@gmail.com>
2020-04-01 13:00:08 +0200

    final inprovements to hypothesis histogram

--- AD/hipothesis.py
+++ AD/hipothesis.py
@@ -15,13 +15,13 @@ import matplotlib.pyplot as plt
 #define statistical significance
 alpha = 0.05 
 
-df = pd.read_csv('datasets/Births.csv', sep=",", index_col=0)
-value_to_compare = 10000
-key = "births"
+# df = pd.read_csv('datasets/Births.csv', sep=",", index_col=0)
+# value_to_compare = 10000
+# key = "births"
 
-# df = pd.read_csv('datasets/manaus.csv', sep=",", index_col=0)
-# key = "manaus"
-# value_to_compare = 0
+df = pd.read_csv('datasets/manaus.csv', sep=",", index_col=0)
+key = "manaus"
+value_to_compare = 0
 
 # df = pd.read_csv('datasets/quakes.csv', sep=",", index_col=0)
 # key = "depth"
@@ -47,21 +47,30 @@ def calculate_coef(standard_deviation, samples):
 def test_hypothesis(min_value, max_value, to_compare):
     return min_value <= to_compare <= max_value
 
-def create_histogram(col, min_value, max_value, to_compare, result, hypothesis_result):
+def create_histogram(col, min_value, max_value, to_compare, result, standard_devaition, hypothesis_result):
     min_interval = min_value
     max_interval = max_value
-    col.plot.hist(bins=100, color='green')
+    n, bins, patches = py.hist(col, 100, density=True, facecolor='green', alpha=0.75)
+    bincenters = 0.5*(bins[1:]+bins[:-1])
+    y = st.norm.pdf( bincenters, loc = result, scale = standard_deviation)
+    l = py.plot(bincenters, y, 'r--', linewidth=1)
     plt.axvline(to_compare, color='red', label="hypothesis value")
     plt.axvline(min_interval, color='orange', label="calculated value margin", linestyle='dashed')
     plt.axvline(max_interval, color='orange', linestyle='dashed')
     plt.axvspan(min_interval, max_interval, alpha=0.2, color='orange')
     plt.axvline(result, color='orange', label="calculated value")
     plt.legend(loc="upper left", fontsize="x-small")
-    plt.text(value_to_compare, 0.5, f"Hypothesis result: {hypothesis_result}", bbox=dict(facecolor='white', alpha=0.5), fontsize="x-small")
+    plt.text(value_to_compare, 0, f"Hypothesis result: {hypothesis_result}", bbox=dict(facecolor='white', alpha=0.5), fontsize="x-small")
     plt.xlabel('depth')
     plt.title("Histogram")
     plt.show()
 
+def histfit(col):  
+    n, bins, patches = py.hist(col, 100, density=True, facecolor='green', alpha=0.75)
+    bincenters = 0.5*(bins[1:]+bins[:-1])
+    y = st.norm.pdf( bincenters, loc = np.mean(col), scale = np.std(col))
+    l = py.plot(bincenters, y, 'r--', linewidth=1)
+    plt.show()
 
 sample_no = get_sample_no(df)
 mean_value = calculate_average(df, key, sample_no)
@@ -74,8 +83,8 @@ hypothesis_result = test_hypothesis(min_value, max_value, value_to_compare)
 
 print("Average {key}: ", mean_value)
 print("standard deviation: ", standard_deviation)
-create_histogram(df[key], min_value, max_value, value_to_compare, mean_value, hypothesis_result)
-
+create_histogram(df[key], min_value, max_value, value_to_compare, mean_value, standard_deviation, hypothesis_result)
+#histfit(df[key])
 
 
 
