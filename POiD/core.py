cc6dcda363f9a47a75e687bb86708999022d43a6
Paulina Wyrwas <paulina.p.wyrwas@gmail.com>
2020-05-16 18:49:39 +0200

    POiD - fft implementation and spectrum plot

--- POiD/core.py
+++ POiD/core.py
@@ -4,8 +4,16 @@ import basic_operations
 import utils
 import histogram
 import filters
+import fft
+
+import numpy as np
+from skimage import color, io
+import matplotlib.pyplot as plt
+
 
 img = utils.load_image('girl.bmp')
+img_grayscale = color.rgb2gray(io.imread('girl.bmp'))
+
 hist = histogram.create_histogram(img)
 
 
@@ -38,15 +46,35 @@ def median_filter_change(lvl):
 
 
 # try:
-cv2.namedWindow('image')
-cv2.createTrackbar('contrast','image',10,20, contrast_param_change)
-cv2.createTrackbar('brightness','image', 256,512, brightness_param_change)
-cv2.createTrackbar('negative','image', 0,1, negative_switch)
-cv2.createTrackbar('average_filter','image', 0,3, average_filter_change)
-cv2.createTrackbar('median_filter','image', 0,3, median_filter_change)
-utils.display_image('image',img,hist)
-hist.show()
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# cv2.namedWindow('image')
+
+#------------------------ EXERCISE 1 --------------------------------
+# cv2.createTrackbar('contrast','image',10,20, contrast_param_change)
+# cv2.createTrackbar('brightness','image', 256,512, brightness_param_change)
+# cv2.createTrackbar('negative','image', 0,1, negative_switch)
+# cv2.createTrackbar('average_filter','image', 0,3, average_filter_change)
+# cv2.createTrackbar('median_filter','image', 0,3, median_filter_change)
+# utils.display_image('image',img,hist)
+# hist.show()
+
+#------------------------ EXERCISE 2 --------------------------------
+x = np.matrix([[1,2,1],[2,1,2],[0,1,1]])
+print(img_grayscale.shape)
+X, m, n = fft.fft2(img_grayscale)
+fshift = fft.fftshift(X)
+magnitude_spectrum = 20*np.log(np.abs(fshift))
+print('\nDFT is :')
+print(X)
+print('\nOriginal signal is :')
+print(fft.ifft2(X, m, n))
+
+plt.subplot(121),plt.imshow(img_grayscale, cmap = 'gray')
+plt.title('Input Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(magnitude_spectrum, cmap = 'gray')
+plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
+plt.show()
+
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
 # except Exception as e:
 #     print(f"Operation failed: {e}")
